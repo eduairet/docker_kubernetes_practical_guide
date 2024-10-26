6645ce99b9ad0e53e11ffe28ea7f8e48060b055f
eduairet <hola@eduairet.com>
2024-10-26 15:31:39 -0600

    Clean Frontend

--- exercices/10-containerized-fullstack-app/frontend/src/App.tsx
+++ exercices/10-containerized-fullstack-app/frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [goals, setGoals] = useState<string[]>([]);
+  const [newGoal, setNewGoal] = useState('');
+
+  const addGoalHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    setGoals(prevGoals => [...prevGoals, newGoal]);
+    setNewGoal('');
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>Docker Goals</h1>
+      <form onSubmit={addGoalHandler}>
+        <input
+          type='text'
+          value={newGoal}
+          onChange={event => setNewGoal(event.target.value)}
+        />
+        <button>Add Goal</button>
+      </form>
+      <ul>
+        {goals.map((goal, index) => (
+          <li key={index}>{goal}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+export default App;
