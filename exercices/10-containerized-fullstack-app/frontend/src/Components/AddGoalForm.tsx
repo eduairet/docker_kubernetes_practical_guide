1cdba7e78d7e6e42afdb1db6fc13de2f295c0d90
eduairet <hola@eduairet.com>
2024-11-06 00:17:09 -0600

    Style updates

--- exercices/10-containerized-fullstack-app/frontend/src/Components/AddGoalForm.tsx
+++ exercices/10-containerized-fullstack-app/frontend/src/Components/AddGoalForm.tsx
@@ -16,14 +16,14 @@ export default function AddGoalForm() {
       <div className='mb-5'>
         <label
           htmlFor='goal'
-          className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+          className='block mb-2 text-sm font-medium text-white'
         >
           New Goal
         </label>
         <input
           type='text'
           id='goal'
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500'
           placeholder='Learn about Docker Volumes'
           value={newGoal}
           onChange={event => setNewGoal(event.target.value)}
@@ -32,7 +32,7 @@ export default function AddGoalForm() {
       </div>
       <button
         type='submit'
-        className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+        className='text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800'
       >
         Add Goal
       </button>
--- exercices/10-containerized-fullstack-app/frontend/src/Components/GoalItem.tsx
+++ exercices/10-containerized-fullstack-app/frontend/src/Components/GoalItem.tsx
@@ -31,14 +31,14 @@ export default function GoalItem({ goal, isEditing, setIsEditing }: IProps) {
       {isEditing ? (
         <input
           type='text'
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500'
           value={newGoal}
           onChange={event => setNewGoal(event.target.value)}
           onKeyDown={handleUpdate}
         />
       ) : (
         <p
-          className='text-sm font-medium text-gray-200 truncate cursor-pointer flex-grow hover:text-blue-500 hover:transition-colors'
+          className='text-sm font-medium text-white truncate cursor-pointer flex-grow hover:text-blue-500 hover:transition-colors'
           onClick={() => setIsEditing(goal.id)}
         >
           {goal.text}
@@ -46,7 +46,7 @@ export default function GoalItem({ goal, isEditing, setIsEditing }: IProps) {
       )}
       <button onClick={handleDelete}>
         <svg
-          className='w-4 h-4 text-blue-500'
+          className='w-4 h-4 text-blue-500 hover:text-red-500 transition-colors'
           aria-hidden='true'
           xmlns='http://www.w3.org/2000/svg'
           width='24'
@@ -62,6 +62,7 @@ export default function GoalItem({ goal, isEditing, setIsEditing }: IProps) {
             d='M6 18 17.94 6M18 18 6.06 6'
           />
         </svg>
+        <span className='sr-only'>Delete goal</span>
       </button>
     </li>
   );
